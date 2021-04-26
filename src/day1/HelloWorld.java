343cf72aeb653ef294c3d87681f4ded37b36ee37
Rovshan Aliyev <raliyev831@yahoo.com>
2021-04-26 15:46:50 -0700

    added the last one

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Adding GitHub Now");
         System.out.println("stuff is in github");
+        System.out.println("we gonna push one more time");
     }
 }
